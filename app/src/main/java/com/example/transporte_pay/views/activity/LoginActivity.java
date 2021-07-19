394df0a21983ff81e875f3227749b67ffe43d7c4
Kisidi <kcalulo@gmail.com>
2021-07-19 19:02:21 +0800

    Added Register UI

--- app/src/main/java/com/example/transporte_pay/views/activity/LoginActivity.java
+++ app/src/main/java/com/example/transporte_pay/views/activity/LoginActivity.java
@@ -1,17 +1,28 @@
 package com.example.transporte_pay.views.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.transporte_pay.R;
 
 public class LoginActivity extends AppCompatActivity {
+    TextView reg_link;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        reg_link = findViewById(R.id.register_link);
+
+        reg_link.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+            startActivity(intent);
+        });
+
     }
 
 
---
+++ app/src/main/java/com/example/transporte_pay/views/activity/RegisterActivity.java
@@ -0,0 +1,30 @@
+package com.example.transporte_pay.views.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.transporte_pay.R;
+
+public class RegisterActivity extends AppCompatActivity {
+    TextView loginLink;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        loginLink = findViewById(R.id.login_link);
+
+        loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
