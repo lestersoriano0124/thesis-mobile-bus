4061f464c5e8bc6c20d6fb505aba27f4923673a4
Kisidi <kcalulo@gmail.com>
2021-07-19 14:31:38 +0800

    first commit

---
+++ app/src/main/java/com/example/transporte_pay/views/activity/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.example.transporte_pay.views.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.transporte_pay.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+ 
+}
